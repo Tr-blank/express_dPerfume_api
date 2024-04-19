ad6bd7cc0102a11d660b1fefa5f2fe23bb4e8ab2
Tr-blank <blank0129@gmail.com>
2024-04-19 21:39:28 +0800

    feat: install mongoose and connect mongoDB

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require('express')
+const mongoose = require('mongoose');
 const app = express()
 const port = 3000
+
+// const DB = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD);
+const DB = 'mongodb://localhost:27017'
+mongoose
+  .connect(DB) // 連線資料庫
+  .then(() => {
+    console.log('資料庫連線成功');
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 app.get('/', (req, res) => {
   res.send('1234')
