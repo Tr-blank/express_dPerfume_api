ca852584fc87512d822d4911138b648c9ad6e59b
Tr-blank <blank0129@gmail.com>
2024-04-20 01:13:03 +0800

    feat: add cors

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express')
+const cors = require('cors');
 const connectDbMiddleware = require('./middlewares/connectDB');
 const indexRouter = require('./routes');
 const perfumesRouter = require('./routes/perfumes');
 const categoriesRouter = require('./routes/categories');
 const app = express()
-const port = 3000
 
+app.use(cors());
 app.use(express.json());
 app.use(connectDbMiddleware);
 
@@ -13,4 +14,4 @@ app.use('/', indexRouter);
 app.use('/perfumes', perfumesRouter);
 app.use('/categories', categoriesRouter);
 
-app.listen(process.env.PORT || port)
+app.listen(process.env.PORT || 3000);
